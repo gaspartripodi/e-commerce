6bc2c9d9079acc685216eff19caaee258a4c07b6
gaspartripodi <gaspartdi@gmail.com>
2020-03-14 00:24:51 -0300

    First commit

---
+++ e-commerce/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+//Components
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { NavComponent } from './nav/nav.component';
+import { StoreComponent } from './store/store.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: StoreComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'cart', component: CartComponent },
+      { path: 'shipping', component: ShippingComponent }
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    NavComponent,
+    StoreComponent,
+    ProductAlertsComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    ShippingComponent
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
---
+++ e-commerce/src/app/products.ts
@@ -0,0 +1,20 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    image: "https://img.cafago.com/product/xy/500/500/p/gu1/P/U/PZ0112G-EU/PZ0112G-EU-1-9a00-G46J.jpg"
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    image: "https://img.cafago.com/product/xy/500/500/p/gu1/P/U/PZ0112G-EU/PZ0112G-EU-1-9a00-G46J.jpg"
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: '',
+    image: ""
+  }
+];
